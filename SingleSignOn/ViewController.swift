d5bcccc07263ce207a47e9af67539cb4b57cc39f
Evelyn Hasama <ehasama11@gmail.com>
2021-01-13 15:43:42 -0800

    Initial Commit

---
+++ SingleSignOn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SingleSignOn
+//
+//  Created by Evelyn Hasama on 1/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
